848f6d4ddb04dbdac68315bd78901f96cdbe072d
Nicolas Medina <nmedinab@unbosque.edu.co>
2017-12-04 01:40:55 -0500

    recaudo Auditoria

---
+++ application/controllers/recaudo_auditoria.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Recaudo_auditoria extends CI_Controller{
+
+	public function __construct(){
+		parent::__construct();
+
+		$this->load->model("Recaudo_auditoria_model");
+      	$this->load->helper('url');
+      	$this->load->helper('form');
+
+
+	}
+
+function index(){
+	
+  	$resultado = $this->Recaudo_auditoria_model->all_auditoria();
+	$data = array("consulta" => $resultado);
+	$this->load->view("header/header");
+  	$this->load->view("view_recaudo_auditoria",$data);
+  	$this->load->view("footer/footer");
+
+
+}
+
+
+}
+
+?>
