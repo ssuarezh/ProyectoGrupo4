feb397cc236d871cab0c9755eebc5dfc736a100d
Santiago Suarez <ssuarezh@unbosque.edu.co>
2017-12-03 19:13:39 -0500

    Actualización transformaciones

--- application/views/contable/view_contable.php
+++ application/views/contable/view_contable.php
@@ -1,15 +1,12 @@
-	
-
-	<!-- section: team -->
+		<!-- section: team -->
 	<section id="contable" class="section green" >
 		<div class="container" >
             <h4>Sección contable</h4>
             	<form action = "<?= base_url()?>index.php/actualizarInfoAfiliado/info" method="post">
-
-
-            		<input type="submit" value="Actualizar información afiliado" >
+               
             	</form>
             	<a href="<?= base_url()?>index.php/contable_auditoria"> <input type="submit" value="Ver Auditoria" > </a>
+          
 		</div>
 		<!-- /.container -->	
 	</section>
---
+++ transformacion_envio_datos/cruce_informacion.bat
@@ -0,0 +1,4 @@
+cd "C:\Pentaho\data-integration"
+call Kitchen.bat/file:C:\AppServ\www\ProyectoGrupo4\transformacion_envio_datos\job_envio_actualizacion_datos.kjb
+pause
+exit
